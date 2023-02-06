d60fb8762e8ecb45cc62a8c4ba2845c6947b21cc
NeelamK1990 <neelamkatwal606@gmail.com>
2023-02-06 11:00:47 -0500

    Created a new component and added form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./logo.png";
-
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"></img>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
