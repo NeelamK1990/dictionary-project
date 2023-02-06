3f4d0a00f27705e02ad344af2384759eca7d784b
NeelamK1990 <neelamkatwal606@gmail.com>
2023-02-06 15:22:45 -0500

    Rendered the word meanings

---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partsOfSpeech}</h3>
+      {props.meaning.definations.map(function (defination, index) {
+        return (
+          <div key={index}>
+            <p>
+              {defination.defination}
+              <br />
+              <em>{defination.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
